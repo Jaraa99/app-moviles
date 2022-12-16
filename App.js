d6b4da026e3129cb02e1558555490d70a90a6554
JCJARAA99 <91167214+Jaraa99@users.noreply.github.com>
2022-12-15 21:38:28 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+
+const PlaceholderImage = require('./assets/images/argentina.jpeg');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <View style={styles.imageContainer}>
+     <ImageViewer placeholderImageSource={PlaceholderImage}/>
+    </View>
+    <View style={styles.footerConteiner}>
+      <Button theme="primary" label="Francia"/>
+      <Button theme="primary"  label="Argentina"/>
+      <Button theme="primary"  label="Croacia"/>
+    </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  image: {
+    width: 200,
+    height: 300,
+    borderRadius: 10,
+  },
+  footerConteiner: {
+    flex: 2 / 3,
+    alignItems: 'center',
+  },
+
+
+});
